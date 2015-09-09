69b7271ba3b75d22523bec907ac99bef7f60bc3f
Sarygid <gid.sary.20@gmail.com>
2015-09-09 14:39:08 +0530

    some changes made

--- wordcount.py
+++ wordcount.py
@@ -1,13 +1,10 @@
-#!/usr/bin/python -tt
+
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
 
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
-
-"""Wordcount exercise
-Google's Python class
 
 The main() below is already defined and complete. It calls print_words()
 and print_top() functions which you write.
